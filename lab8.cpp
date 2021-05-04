358f356a2fdc01929f09100cdb64f3b276d43289
OlegOzatsky <79273488+OlegOzatsky@users.noreply.github.com>
2021-05-04 12:04:16 +0600

    Add files via upload

---
+++ lab8.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class Myclass
+{
+public:
+    double x;
+    double y;
+
+    void getCos()
+    {
+        cout << "cos z = " << cos(x) * (pow(exp(1), y) + pow(exp(1), -y) / 2) << " - i" << sin(y) * (pow(exp(1), y) - pow(exp(1), -y) / 2);
+    }
+};
+
+double getValue(char n)
+{
+    while (true)
+    {
+        double value;
+        cout << "Введите " << n << ": ";
+        cin >> value;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(32767, '\n');
+            cout << n << " - число. \n";
+        }
+        else
+        {
+            cin.ignore(32767, '\n');
+            return value;
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    Myclass m;
+    m.x = getValue('x');
+    m.y = getValue('y');
+    m.getCos();
+    return 0;
+}
